adc881752f25bca7c96efdd7b6d0ce0bf72b6790
sakme <alan@akme.us>
2022-01-30 13:04:38 -0500

    finished comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,11 +4,54 @@ const { User, Post, Comment } = require('../../models');
 
 
 router.get('/', (req, res) => {
-    
+    Comment.findAll({
+      attributes: ['comment_text','created_at'],
+      include: [
+        {
+          model: Post,
+          attributes: ['title','post_text','created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        }
+      ]
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
-
+  Comment.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['comment_text','created_at'],
+      include: [
+        {
+          model: Post,
+          attributes: ['title','post_text','created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        }
+      ]
+  })
+  .then(dbCommentData => {
+    if (!dbCommentData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.json(dbCommentData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
@@ -25,11 +68,45 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-
+  Comment.update(req.body, 
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(dbCommentData => {
+    if (!dbCommentData) {
+      res.status(404).json({ message: 'No comment found with this id' });
+      return;
+    }
+    res.json(dbCommentData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
-
+  Comment.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(dbCommentData => {
+    if (!dbCommentData) {
+      res.status(404).json({ message: 'No comment found with this id' });
+      return;
+    }
+    res.json(dbCommentData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
