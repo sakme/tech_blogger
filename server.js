265a98865b93c846fa4666dffcc72744d84c8eef
sakme <alan@akme.us>
2022-01-30 16:51:59 -0500

    setup homepage

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const path = require('path');
+const path = require('path');
 // const helpers = require('./utils/helpers');
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({ helpers });
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 // const session = require('express-session');
@@ -22,9 +22,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 // app.use(session(sess));
 app.use(routes);
 
