d3949ca81222f7d04c1b07afc5ce43062a6c2932
sakme <alan@akme.us>
2022-02-04 16:07:14 -0500

    edit delete and session timeout

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 600000,
+    rolling: true
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
